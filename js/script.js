c2c007b18dc7975ee06d06c7d8a407b6b4addbca
Syafiq <syafiqcs003@gmail.com>
2024-09-14 08:13:31 +0700

    lanjuttt

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function changeBg(bg, title) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  changeBg("bg-the-covenant.jpeg", "the-covenant");
+  changeBg("bg-My-bio.jpeg", "my-bio");
 
   document.querySelector(".trailer").style.display = "none";
 });
